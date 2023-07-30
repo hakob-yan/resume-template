d3384e9c5556c3878045341e6f40f7289e21069b
hakob-yan <devhakob@gmail.com>
2023-07-30 15:12:27 +0400

    added variables for colors

--- src/components/Languages/index.js
+++ src/components/Languages/index.js
@@ -1,12 +1,51 @@
 import React from "react";
 import "./styles.scss";
+const languagesArr = [
+  {
+    language: "English",
+    level: 4,
+    desc: "Proficient",
+  },
+  {
+    language: "Russian",
+    level: 4,
+    desc: "Proficient",
+  },
+  {
+    language: "Armenian",
+    level: 5,
+    desc: "Native",
+  },
+];
+const LngLevel = ({ level }) => {
+  return (
+    <div className="lang-level">
+      {[0, 0, 0, 0, 0].map((el, i) => {
+        return <div className={`level ${i<level  ? "active" : ""}`}></div>;
+      })}
+    </div>
+  );
+};
+
+const LangElem = ({ lang }) => {
+  return (
+    <div className="lang-item">
+      <p>{lang.language}</p>
+      <p>{lang.desc}</p>
+      <LngLevel level={lang.level} />
+    </div>
+  );
+};
 
 function Languages() {
   return (
     <div className="Languages section">
       <p className="section-title">Languages</p>
-      <div className="topic">Languages</div>
-
+      <div className="topic language-secion">
+        {languagesArr.map((lang) => {
+          return <LangElem lang={lang} />;
+        })}
+      </div>
     </div>
   );
 }
