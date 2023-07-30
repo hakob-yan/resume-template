3b4590331f69b6b6bae747be87e0e02f5f7c3826
hakob-yan <devhakob@gmail.com>
2023-07-30 14:10:23 +0400

    added skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,11 +1,52 @@
 import React from "react";
 import "./styles.scss";
+const basicScillsArr = [
+  "HTML5",
+  "CSS3",
+  "JavaScript(ES6+)",
+  "TypeScript",
+  "React",
+  "Redux(Thunk)",
+  "MobX",
+  "Next.js",
+  "Apollo GraphQL"
+];
+const librariesArr = [
+  "Sass/SCSS",
+  "Tailwind CSS",
+  "Styled Components",
+  "Material UI",
+  "Bootstrap 5",
+];
+const toolssArr = ["Ajax","RESTful APIs", "OOP","Git","Docker","Webpack","Babel"];
 
 function Skills() {
   return (
     <div className="Skills section">
       <p className="section-title">Skills</p>
-      <div className="topic">Skills</div>
+      <div className="topic skills_section">
+        <div className="first-part-skills">
+          <ul className="topic-desc">
+            {basicScillsArr.map((skill) => (
+              <li>{skill}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="second-part-skills">
+          <ul className="topic-desc">
+            {librariesArr.map((skill) => (
+              <li>{skill}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="third-part-skills">
+          <ul className="topic-desc">
+            {toolssArr.map((skill) => (
+              <li>{skill}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
