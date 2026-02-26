3e8207ffb956310bf93080ba2b3418d98e24c90b
Hakob <devhakob@gmail.com>
2026-02-26 23:31:14 +0400

    new data

--- client-senior-position/src/components/Provider/index.tsx
+++ client-senior-position/src/components/Provider/index.tsx
@@ -2,7 +2,7 @@ function Provider({ children }: { children: React.ReactNode }) {
   return (
     <div
       id="resume-wrapper"
-      className="max-w-300 h-424.5 mx-auto w-full mt-36 bg-white shadow-[-10px_20px_60px_5px_rgba(0,0,0,0.25)]   rounded-xs   overflow-hidden"
+      className="max-w-300 print:max-w-full h-424.5 print:h-auto mx-auto w-full mt-36 print:mt-0  bg-white shadow-[-10px_20px_60px_5px_rgba(0,0,0,0.25)]  print:shadow-none rounded-xs print:rounded-none  overflow-hidden"
     >
       {children}
     </div>
